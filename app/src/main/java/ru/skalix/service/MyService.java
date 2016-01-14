470058516cd64aacf705e9a6e605df60e4605c2e
Kirill S <skalix@gf.tom.ru>
2016-01-15 01:09:43 +0600

    Add TCP-Client; Add Widget;

--- app/src/main/java/ru/skalix/service/MyService.java
+++ app/src/main/java/ru/skalix/service/MyService.java
@@ -3,11 +3,19 @@ package ru.skalix.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyService extends Service {
-    public MyService() {
-    }
+    static String data;
+    static boolean trigger = true;
+    private TCPC client;
+    private Thread conn;
+    private Timer tmr;
+    private MTimerTask mtm;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -16,23 +24,52 @@ public class MyService extends Service {
     }
 
     @Override
-    public void onCreate()
-    {
-        Toast.makeText(this, "Служба создана",
-                Toast.LENGTH_SHORT).show();
+    public void onCreate() {
+        tmr = new Timer();
+        mtm = new MTimerTask();
+
+        client = new TCPC("127.0.0.1", 2007);
+        conn = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (client != null && trigger) {
+                        if (client.Connect()) {
+                            Log.d("myLogs", "Connected!");
+                            client.Send("GET");
+                            data = client.Read();
+                            client.Disconnect();
+                            trigger = false;
+                        } else {
+                            Log.d("myLogs", "Failed connect!");
+                        }
+                    }
+                }
+            }
+        });
+
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        conn.start();
+        tmr.schedule(mtm, 0, 5000);
+        Toast.makeText(this, "Служба запущена", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
     }
 
     @Override
-    public void onStart(Intent intent, int startid)
-    {
-        Toast.makeText(this, "Служба запущена",
-                Toast.LENGTH_SHORT).show();
+    public void onDestroy() {
+        if (client != null) client.Disconnect();
+        conn.interrupt();
+        tmr.cancel();
+        Toast.makeText(this, "Служба остановлена", Toast.LENGTH_SHORT).show();
     }
+}
+
+class MTimerTask extends TimerTask {
 
     @Override
-    public void onDestroy()
-    {
-        Toast.makeText(this, "Служба остановлена",
-                Toast.LENGTH_SHORT).show();
+    public void run() {
+        MyService.trigger = true;
     }
 }
---
+++ app/src/main/java/ru/skalix/service/TestWidget.java
@@ -0,0 +1,70 @@
+package ru.skalix.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import java.net.URISyntaxException;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class TestWidget extends AppWidgetProvider {
+
+    final String UPDATE_ALL_WIDGETS = "update_all_widgets";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.test_widget);
+        views.setTextViewText(R.id.appwidget_text, MyService.data);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction().equalsIgnoreCase(UPDATE_ALL_WIDGETS)) {
+            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
+            for (int appWidgetId : ids) {
+                updateAppWidget(context, appWidgetManager, appWidgetId);
+            }
+        }
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Intent intent = new Intent(context, TestWidget.class);
+        intent.setAction(UPDATE_ALL_WIDGETS);
+        PendingIntent pIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), 5000, pIntent);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Intent intent = new Intent(context, TestWidget.class);
+        intent.setAction(UPDATE_ALL_WIDGETS);
+        PendingIntent pIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(pIntent);
+    }
+}
+
